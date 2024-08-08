d3c27098f92c222af5fd35e8b1e7d90163dc7f21
Jakub Kolar <jakubkolar211@gmail.com>
2024-08-08 13:58:28 +0200

    Added script for successful imports checking

---
+++ method/test_imports.py
@@ -0,0 +1,8 @@
+print('Testing if all libraries are installed and can be imported successfully... ', end='')
+import main
+import logger
+import ee3p3d
+import loader
+import utils
+print('All import were successful!')
+print('OK')
