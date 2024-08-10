e18a8c1197b3fb5a9c2aa64becda1175f1f095c6
Jakub Kolar <jakubkolar211@gmail.com>
2024-08-10 16:11:36 +0200

    Save to .log with debug level, assert_and_log fn

--- method/logger.py
+++ method/logger.py
@@ -6,19 +6,27 @@ logger = logging.getLogger(__name__)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 
-def setup_logger(log_folder, level):
+def setup_logger(log_folder, console_level):
     # Create the file handler for logging to a file
     log_filename = os.path.join(
-        log_folder, 
+        log_folder,
         f'log_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log')
     file_handler = logging.FileHandler(log_filename)
     file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
 
     # Create the stream handler for logging to the console
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(console_level)
 
     # Add the handlers to the logger
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
-    logger.setLevel(level.upper())
+    logger.setLevel(logging.DEBUG)
+
+
+def assert_and_log(condition, message):
+    if not condition:
+        logger.error(f"Assertion failed: {message}")
+        raise AssertionError(message)
